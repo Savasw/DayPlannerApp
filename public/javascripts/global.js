a53ec45de6225627d533085ad955bbc632020ee8
Savasw <sapan.vaswani@click2cloud.net>
2018-11-15 17:53:26 +0530

    first commit

---
+++ public/javascripts/global.js
@@ -0,0 +1,452 @@
+﻿
+var azureSearch = {
+    
+
+    //Get geo-spatial data from index based on latitude, longitude and radius in km.
+    getDataFromIndex: function (latitude, longitude, radius) {
+        $.ajax({
+            url: "/api/v1/get_data_from_index",
+            type: "get",
+            data: {
+                latitude: latitude,
+                longitude: longitude,
+                radius: radius,
+            },
+            success: function (response) {
+
+                console.log(response.length)
+                if (response.length > 0) {
+                    for (var i = 0; i < response.length; i++) {
+
+                        var meetingContentHTML = '<div>' +
+                            '<div class="infowindow"><p><b>' + response[i].name + '</b></p></div>' +
+                            '<p><span style="font-weight:bold;color:fff">Location</span>: ' + response[i].address + '</p>' +
+                            '</div>';
+                        gMap.display_coffee_shop_marker(new google.maps.LatLng(response[i].location.coordinates[1], response[i].location.coordinates[0]), meetingContentHTML, response[i].name, "")
+                    }
+                    $.unblockUI()
+                } else {
+                    $.unblockUI()
+                    swal('Oops', 'No near by Coffee shops available !!');
+
+                }
+
+            },
+            error: function (xhr) {
+                $.unblockUI()
+                swal('Error', 'Error in getting coffee shops data from Azure Search Service', 'error');
+            }
+        });
+    },
+
+
+}
+var postgres = {
+    meetingData: null,
+    getEngagementsData: function (date) {
+        //display please wait loader on date change/refresh page
+        main.blockUI();
+        $.ajax({
+            url: "/api/v1/GetMeetingData",
+            type: "get",
+            data: {
+                meeting_date: date,
+            },
+            success: function (response) {
+
+                postgres.meetingData = response;
+                gMap.resetMap();
+                // remove please wait loader
+                $.unblockUI()
+                if (response)
+                    gMap.renderRouteData(response);
+                else {
+                    swal('No engagements', 'No engagements on ' + $('.date-picker').data('date'), 'info');
+                    $("#engagementdata").html('<li><a>No Engagements</a></li>')
+                }
+            },
+            error: function (xhr) {
+                //Do Something to handle error
+                // remove please wait loader
+                $.unblockUI()
+                swal('Error', 'Error in getting meeting data.', 'error');
+            }
+        });
+    },
+
+    saveEngagement: function (title, meeting_date, start_time, end_time, loc, latitude, longitude) {
+        //function call to display loader.
+        main.blockUI();
+        // ajaxpost to save new engagement
+        $.ajax({
+            url: '/insert_data',
+            type: 'PUT',
+            data: {
+                title: title,
+                meeting_date: meeting_date,
+                start_time: start_time,
+                end_time: end_time,
+                loc_name: loc,
+                lat: latitude,
+                long: longitude
+            },
+            success: function (data) {
+                //stop loader
+                $.unblockUI()
+                swal('Success', 'Engagement saved successfully.', 'success');
+
+                // reset map after saving new engagement
+                var dateArray = $('.date-picker').data('date').split("/");
+                var selectedMeetingdate = dateArray[2] + "-" + dateArray[0] + "-" + dateArray[1];
+
+                var newEngagementDate = meeting_date.split('-')[0] + "-" + meeting_date.split('-')[1] + "-" + meeting_date.split('-')[2];
+
+                if (selectedMeetingdate == newEngagementDate) {
+                    postgres.getEngagementsData(selectedMeetingdate);
+                }
+            },
+            error: function (err) {
+                //stop loader
+                $.unblockUI()
+                swal('Error', 'Error while saving Engagement.', 'error');
+            }
+        });
+    }
+
+
+}
+
+var gMap = {
+
+    map: null,
+    directionsRendererService: "",
+    directionsService: "",
+    distanceMatrixService: "",
+    //waypts: [],
+    latlongArrayGlobal: [],
+    markers: [],
+    locmarkers: [],
+    infoWindows: [],
+    locinfowindow: [],
+
+    //loads the map and binds it to the provided element and initiates direction components
+    loadMap: function (mapContainer) {
+
+        gMap.map = new google.maps.Map(mapContainer, {
+            zoom: 16,
+            center: { lat: 47.374366, lng: -122.2995801 }
+        });
+
+        gMap.directionsService = new google.maps.DirectionsService;
+        gMap.directionsRendererService = new google.maps.DirectionsRenderer({ suppressMarkers: true });
+
+        //gMap.directionsRendererService.setMap(gMap.map);
+
+    },
+
+    //Clear all the coffee shops marker and it's infowindow.
+    clearCoffeeMarker: function () {
+
+        $(".dropdown li").removeClass("Selected")
+        for (var i = 0; i < gMap.locmarkers.length; i++) {
+            gMap.locmarkers[i].setMap(null);
+            //gMap.markers[i] = null;
+        }
+        for (var i = 0; i < gMap.locinfowindow.length; i++) {
+            gMap.locinfowindow[i].close();
+        }
+        gMap.locinfowindow = [];
+        gMap.locmarkers = [];
+        gMap.map.setZoom(10)
+        $("#title").text("Select Engagement")
+        $("#btndistance li").removeClass("Selected")
+        $("#disttitle").text("Select Range")
+        //$("#btndistance #disttitle").text("5 mi")
+        //$("#btndistance li:eq(1)").addClass("Selected")
+
+    },
+
+    //function to clear map .
+    resetMap: function () {
+        gMap.directionsRendererService.setMap(null);
+        // gMap.directionsRendererService = null;
+        gMap.directionsRendererService = new google.maps.DirectionsRenderer({ suppressMarkers: true });
+        // gMap.directionsRendererService.setMap(gMap.map);
+
+        for (var i = 0; i < gMap.markers.length; i++) {
+            gMap.markers[i].setMap(null);
+            //gMap.markers[i] = null;
+        }
+
+
+        for (var i = 0; i < gMap.infoWindows.length; i++) {
+            gMap.infoWindows[i].close();
+        }
+
+        gMap.markers = [];
+        gMap.infoWindows = [];
+
+        gMap.clearCoffeeMarker();
+    },
+
+    //function to display engagements data on google map.
+    renderRouteData: function (meetingData) {
+
+        //gMap.resetMap();       
+        gMap.directionsRendererService.setMap(gMap.map);
+
+
+        var latlongarray = [];
+
+        var meetingContentArray = [];
+        var locationTitles = [];
+        var str = ""
+        //segregating meeting data for lat-long and meeting content
+        for (var i = 0; i < meetingData.length; i++) {
+            var lat = meetingData[i].geometry.coordinates["1"];
+            var long = meetingData[i].geometry.coordinates["0"];
+
+            latlongarray.push(new google.maps.LatLng(lat, long));
+
+            var meetingContentHTML = '<div>' +
+                '<div class="infowindow"><p><b>' + meetingData[i].title + '</b></p></div>' +
+                '<p><span style="font-weight:bold;color:fff">Start</span>: ' + meetingData[i].start + '</p>' +
+                '<p><span style="font-weight:bold;color:fff">End</span>: ' + meetingData[i].end + '</p>' +
+                '<br/>' +
+                '<p><span style="font-weight:bold;color:fff">Location</span>: ' + meetingData[i].loc_name + '</p>' +
+                '</div>';
+
+            meetingContentArray.push(meetingContentHTML);
+
+            locationTitles.push(meetingData[i].loc_name);
+
+            str += "<li data-lat=" + meetingData[i].geometry.coordinates["1"] + " title=" + meetingData[i].title + " data-lng=" + meetingData[i].geometry.coordinates["0"] + "><a href='#'>";
+            if (meetingData[i].title.length > 37) {
+                str += meetingData[i].title.substring(0, 37) + "...</a></li>";
+            } else {
+                str += meetingData[i].title + "</a></li>";
+
+            }
+        }
+        $("#engagementdata").html(str)
+
+        $(".dropdown li").click(function () {
+
+            $(".dropdown li").removeClass("Selected")
+            $(this).addClass("Selected")
+            $(".dropdown #title").text($(this).text())
+
+            for (var i = 0; i < gMap.locmarkers.length; i++) {
+                gMap.locmarkers[i].setMap(null);
+                //gMap.markers[i] = null;
+            }
+
+            gMap.locmarkers = [];
+            //alert("")
+
+        });
+
+        gMap.latlongArrayGlobal = latlongarray;
+
+        gMap.waypts = []
+        if (latlongarray.length > 2) {
+
+            var waypts = []
+            for (i = 1; i < latlongarray.length - 1; i++) {
+                waypts.push({ location: latlongarray[i], stopover: true })
+
+            }
+
+            gMap.directionsService.route({
+                origin: latlongarray[0],
+                destination: latlongarray[latlongarray.length - 1],
+                waypoints: waypts,
+                optimizeWaypoints: false,
+                travelMode: 'DRIVING'
+            }, function (response, status) {
+                if (status === 'OK') {
+                    console.log(response)
+                    gMap.directionsRendererService.setDirections(response);
+                }
+                //  console.log(response, status)
+            });
+
+        }
+        console.log(latlongarray.length)
+        if (latlongarray.length == 1) {
+            gMap.display_marker(latlongarray[0], meetingContentArray[0], locationTitles[0], 1)
+            gMap.map.setCenter(latlongarray[0], 6)
+        }
+        else {
+            for (var i = 0; i < latlongarray.length - 1; i++) {
+
+                gMap.apply_direction_service(latlongarray[i], latlongarray[i + 1]);
+
+                gMap.display_marker(latlongarray[i], meetingContentArray[i], locationTitles[i], (i + 1))
+
+                if (i == latlongarray.length - 2) {
+                    gMap.display_marker(latlongarray[i + 1], meetingContentArray[i + 1], locationTitles[i + 1], (i + 2))
+                }
+            }
+        }
+    },
+
+    //uses direction service to get middle location of route between provided source and dest - middle location is used to show distnace and hours between source and dest
+    apply_direction_service: function (sourcelatlong, destlatlong) {
+        var middle = "";
+        gMap.directionsService.route({
+            origin: sourcelatlong,
+            destination: destlatlong,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        }, function (response, status) {
+            if (status === 'OK') {
+                if (gMap.latlongArrayGlobal.length < 3) {
+                    gMap.directionsRendererService.setDirections(response);
+
+                    if (gMap.latlongArrayGlobal.length == 2 && sourcelatlong.lat() == destlatlong.lat() && sourcelatlong.lng() == destlatlong.lng()) {
+                        setTimeout(function () {
+                            //  gMap.directionsRendererService.setMap(gMap.map);
+                            gMap.map.setZoom(10)
+                        }, 100);
+                    }
+                }
+                var m = Math.ceil((response.routes[0].overview_path.length) / 2)
+                middle = response.routes[0].overview_path[m]
+
+                if (sourcelatlong.lat() != destlatlong.lat() && sourcelatlong.lng() != destlatlong.lng())
+                    gMap.apply_direction_matrix(middle, sourcelatlong, destlatlong)
+                //callback(middle, sourcelatlong, destlatlong);
+            }
+            else {
+
+                swal('Error', 'Direction request failed due to ' + status, 'error');
+            }
+        });
+    },
+
+
+
+    //use distance matrix service to get distance and travel time between source and destination.
+    apply_direction_matrix: function (middle, sourcelatlong, destlatlong) {
+
+        var service = new google.maps.DistanceMatrixService;
+        service.getDistanceMatrix({
+            origins: [sourcelatlong],
+            destinations: [destlatlong],
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.IMPERIAL
+        }, function (response, status) {
+            console.log(response)
+            if (status === 'OK') {
+                var originlist = response.originAddresses;
+                var destinationllist = response.destinationAddresses;
+                for (var i = 0; i < originlist.length; i++) {
+                    var results = response.rows[i].elements;
+                    for (var j = 0; j < results.length; j++) {
+                        var element = results[j]
+                        var dt = element.distance.text;
+                        var dr = element.duration.text;
+                    }
+                }
+
+                //callback(middle, dt, dr);
+                gMap.displayinfowindow(middle, dt, dr)
+
+            } else {
+                swal('Error', 'Distance matrix request failed due to ' + status, 'error');
+            }
+        });
+
+    },
+
+    //function to display engagements marker on google map.
+    display_marker: function (latlong, displayContentHTML, markerTitle, index) {
+
+        //var contentString = '<div id="content">' +
+        //    '<div id="siteNotice"><h2>Meeting Content</h2>' +
+        //    '</div>' +
+
+        //    '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: displayContentHTML
+        });
+
+        var marker = new google.maps.Marker({
+            position: latlong,
+            map: gMap.map,
+            title: markerTitle,
+            label: "" + index + ""
+        });
+        gMap.markers.push(marker);
+
+        marker.addListener('click', function () {
+            infowindow.open(gMap.map, marker);
+        });
+    },
+
+    //display coffee shops marker on google map.
+    display_coffee_shop_marker: function (latlong, displayContentHTML, markerTitle, index) {
+
+        var infowindow = new google.maps.InfoWindow({
+            content: displayContentHTML
+        });
+        gMap.locinfowindow.push(infowindow);
+
+        var marker = new google.maps.Marker({
+            position: latlong,
+            map: gMap.map,
+            title: markerTitle,
+            icon: {
+                url: 'images/Coffee_2.png',
+                scaledSize: new google.maps.Size(50, 50)
+            },
+        });
+
+        gMap.locmarkers.push(marker);
+
+
+        marker.addListener('click', function () {
+            infowindow.open(gMap.map, marker);
+        });
+    },
+
+
+
+    //displays infowindow
+
+    displayinfowindow: function (latlong, dt, dr) {
+        var content = '<div><i class="fa fa-car" aria-hidden="true" style="margin-right:3px;"></i>' + dt +
+            '<br>' + dr +
+            '</div>';
+        var infowindow1 = new google.maps.InfoWindow({
+            content: content
+        });
+        //infowindow1.setContent(content)
+        infowindow1.setPosition(latlong)
+        infowindow1.open(gMap.map)
+
+        gMap.infoWindows.push(infowindow1);
+
+    },
+
+    //function for google map location autocomplete
+    geolocate: function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var circle = new google.maps.Circle({
+                    center: geolocation,
+                    radius: position.coords.accuracy
+                });
+                autocomplete.setBounds(circle.getBounds());
+
+            });
+        }
+
+    }
+}
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,255 @@
+﻿var mapContainer;
+var meetingDate;
+
+var loc = "";
+var latitude = "";
+var longitude = "";
+
+var selected_start_time = "";
+var selected_end_time = "";
+
+$(document).ready(function () {
+
+    gMap.loadMap(document.getElementById('map'));
+    mapContainer = $('#map');
+
+    //setting today as current date in date picker
+    var date = new Date();
+    var today = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+    $("#date-input").val(today);
+
+    main.initiateAddEngagementControls();
+    main.initiateSearchControls();
+
+    $('.input-datepicker > .input-group-btn > button').click();
+
+    $("#date-input").on('keyup', function (e) {
+        if (e.keyCode == 13) {
+            $('.input-datepicker > .input-group-btn > button').click();
+        }
+    });
+});
+
+
+var main = {
+
+
+    initiateAddEngagementControls: function () {
+        $("#saveengagement").click(function () {
+            main.saveEngagement();
+        });
+
+        $("#newEventModel").click(function () {
+            main.resetEngagementControls();
+        });
+
+        $(".form_time[data-link-field='starttime-name']").change(function () {
+            // set start time value in selected_start_time variable
+            selected_start_time = "";
+            if ($("#starttime-name").val().trim() != "") {
+                var d = new Date();
+                var day = d.getDay();
+                var month = d.getMonth();
+                var year = d.getFullYear();
+                var hour = $("#starttime-name").val().split(':')[0];
+                var min = $("#starttime-name").val().split(':')[1];
+                selected_start_time = new Date(year, month, day, hour, min);
+                if ($("#endtime-name").val().trim() != "") {
+                    var d = new Date();
+                    var day = d.getDay();
+                    var month = d.getMonth();
+                    var year = d.getFullYear();
+                    var hour = $("#endtime-name").val().split(':')[0];
+                    var min = $("#endtime-name").val().split(':')[1];
+                    var selected_end_time = new Date(year, month, day, hour, min)
+                    if (selected_end_time <= selected_start_time) {
+                        swal('Warning', 'Start time should be less than End time', 'warning');
+                        $(".form_time[data-link-field='starttime-name'] .glyphicon-remove").click()
+
+                    }
+                }
+            }
+        });
+
+        $(".form_time[data-link-field='endtime-name']").change(function () {
+            // set start time value in selected_end_time variable
+            selected_end_time = "";
+            if ($("#endtime-name").val().trim() != "") {
+                var d = new Date();
+                var day = d.getDay();
+                var month = d.getMonth();
+                var year = d.getFullYear();
+                var hour = $("#endtime-name").val().split(':')[0];
+                var min = $("#endtime-name").val().split(':')[1];
+                var selected_end_time = new Date(year, month, day, hour, min)
+            }
+
+            // validate start time and end time
+            if ($("#starttime-name").val().trim() != "" && $("#endtime-name").val().trim() != "") {
+                if (selected_end_time <= selected_start_time) {
+                    swal('Warning', 'End time should be greater than Start time', 'warning');
+                    $(".form_time[data-link-field='endtime-name'] .glyphicon-remove").click()
+                }
+            }
+        });
+
+        // Create Location auto-complete
+        google.maps.event.addDomListener(window, 'load', function () {
+            var places = new google.maps.places.Autocomplete(document.getElementById('location'));
+            google.maps.event.addListener(places, 'place_changed', function () {
+                var place = places.getPlace();
+                loc = $("#location").val();
+                latitude = place.geometry.location.lat();
+                longitude = place.geometry.location.lng();
+            });
+        });
+
+        // Create Datepicker control
+        $('.form_date').datetimepicker({
+            language: 'fr',
+            weekStart: 1,
+            todayBtn: 1,
+            autoclose: 1,
+            todayHighlight: 1,
+            startView: 2,
+            minView: 2,
+            forceParse: 0
+        });
+
+        // Create Timepicker control
+        $('.form_time').datetimepicker({
+            language: 'fr',
+            weekStart: 1,
+            todayBtn: 1,
+            autoclose: 1,
+            todayHighlight: 1,
+            startView: 1,
+            minView: 0,
+            maxView: 1,
+            forceParse: 0
+        });
+
+        // set current date to meeting date datepicker
+        $('.form_date').datetimepicker('setDate', new Date);
+
+        $("#newEventModel").click(function (event) {
+            event.stopPropagation();
+            $("#myModal").modal('show');
+            return false;
+        });
+    },
+
+    initiateSearchControls: function () {
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $("#btndistance li").click(function () {
+            $("#btndistance #disttitle").text($(this).text())
+            $("#btndistance li").removeClass("Selected")
+            $(this).addClass("Selected")
+        })
+
+        //create index in azure search service.
+        // azureSearch.createIndex()
+
+        //clear function of coffee shops
+        $("#btnclear").click(function () {
+            gMap.clearCoffeeMarker();
+        })
+        //click event of search coffee shops
+        $("#btnazuresearch").click(function () {
+            if ($(".dropdown li.Selected").length > 0) {
+                if ($("#btndistance li.Selected").length > 0) {
+                    main.blockUI();
+                    for (var i = 0; i < gMap.locmarkers.length; i++) {
+                        gMap.locmarkers[i].setMap(null);
+                        //gMap.markers[i] = null;
+                    }
+                    for (var i = 0; i < gMap.locinfowindow.length; i++) {
+                        gMap.locinfowindow[i].close();
+                    }
+                    gMap.locinfowindow = [];
+                    gMap.locmarkers = [];
+                    //gMap.map.center(new google.maps.LatLng(lat, long))
+                    gMap.map.setCenter(new google.maps.LatLng($(".dropdown li.Selected").attr("data-lat"), $(".dropdown li.Selected").attr("data-lng")));
+                    gMap.map.setZoom(14)
+                    azureSearch.getDataFromIndex($(".dropdown li.Selected").attr("data-lat"), $(".dropdown li.Selected").attr("data-lng"), $("#btndistance li.Selected").attr("data-value"))
+                } else {
+                    swal('Error', 'Select Range for Azure Geospatial Search', 'error');
+                }
+            }
+            else {
+                swal('Error', 'Select Engagement for Azure Geospatial Search', 'error');
+            }
+        })
+    },
+
+    saveEngagement: function () {
+        var title = $("#title-name").val();
+        var meeting_date = $("#meetingdate-name").val();
+        var start_time = $("#starttime-name").val();
+        var end_time = $("#endtime-name").val();
+
+        if (title.trim() == "") {
+            swal('Warning', 'Please enter title.', 'warning');
+        }
+        else if ($("#location").val().trim() == "") {
+            swal('Warning', 'Please select location.', 'warning');
+        }
+        else if (loc.trim() != $("#location").val().trim() || latitude == "" || longitude == "") {
+            swal('Warning', 'Please select valid location.', 'warning');
+        }
+        else if (meeting_date.trim() == "") {
+            swal('Warning', 'Please enter meeting date.', 'warning');
+        }
+        else if (start_time.trim() == "") {
+            swal('Warning', 'Please enter start time.', 'warning');
+        }
+        else if (end_time.trim() == "") {
+            swal('Warning', 'Please enter end time.', 'warning');
+        }
+        else {
+            $(".close").click();
+
+            postgres.saveEngagement(title, meeting_date, start_time, end_time, loc, latitude, longitude);
+        }
+    },
+
+    resetEngagementControls: function () {
+        $(".modal-body input").val("");
+        //populating selected date in add engagement date control
+        $('.form_date').datetimepicker('setDate', new Date($('.date-picker').data('date')));
+        loc = "";
+        latitude = "";
+        longitude = "";
+        selected_start_time = "";
+        selected_end_time = "";
+    },
+
+
+    // function to display loader 
+    blockUI: function () {
+
+        $.blockUI({
+            css: {
+                border: 'none',
+                padding: '10px 10px 15px 10px',
+                backgroundColor: '#000',
+                '-webkit-border-radius': '10px',
+                '-moz-border-radius': '10px',
+                opacity: .5,
+                color: '#fff'
+            }
+        });
+        $(".blockMsg h1").css({ 'font-size': '25px' })
+
+    }
+}
+
+
+function initMap() {
+
+}
+
+
+
